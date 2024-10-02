6fa9893cbdca17c977da5f3b7e51f3d901f6eb4d
svidal1987 <svidal1987@gmail.com>
2024-10-02 15:17:19 -0300

    Delete 2c directory

--- 2c/auth0example/src/app/pages/public/error/error.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ErrorComponent } from './error.component';
-
-export const ERROR_ROUTES: Routes = [
-  { path: ':code', component: ErrorComponent },
-];
